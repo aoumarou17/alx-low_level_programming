88f2ddbf4e29abcd3476d3bda6a9e22a72b81cf6
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-15 12:49:47 -0700

    Prinitng alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphaber - function for printing lowercase alphabets
+ */
+void print_alphabet(void);
+{
+	char alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(apha);
+		alpha++
+	}
+
+	_putchar(10);
+}
