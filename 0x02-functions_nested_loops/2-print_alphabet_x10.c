5a1e493a80dfa67693d8c4a133f3d8ae63e9a45a
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-15 13:52:38 -0700

    Print 10 Alphabtes

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Printing alphabets
+ */
+void print_alphabet_x10(void)
+{
+	char alpha = 'a';
+	int n = 10;
+
+	while (n >= 10)
+	{
+		while (alpha <= 'z')
+		{
+			_putchar(alpha);
+			++alpha;
+		}
+		--n;
+		_putchar(alpha);
+		alpha = 'a';
+	}
+}
