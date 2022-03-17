8fcf9d89d29e5bd274b0e7474c865fe680a9c2f4
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-17 01:28:22 -0700

    The test file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,15 +1,17 @@
 #include "main.h"
+
 /**
- * main - Test the function positive/negative
+ * main - tests function that prints if integer is positive or negative
  *
  * Return: 0
  */
+
 int main(void)
 {
-	int x;
+	int i;
 
-	x = 0;
+	i = 0;
 
-	positive_or_negative(x);
+	positive_or_negative(i);
 	return (0);
 }
