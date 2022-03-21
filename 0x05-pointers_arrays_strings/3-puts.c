08d63b26232e454f2745d34810cb906eeb76c336
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-21 12:58:34 -0700

    String

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts - function
+ *
+ * @str: a parameter of the function
+ */
+
+int void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar(10);
+}
