b73af97fe1e8976dfc526ab8d1d1774ba770e0d4
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-21 13:23:09 -0700

    Reverse String

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rev_string - function
+ *
+ * @s: pointer of the parameter
+ */
+void rev_string(char *s)
+{
+	char you;
+	int len = 0;
+	int a;
+	int k;
+
+	while (s[len] != '\0')
+		len++;
+	k = len - 1;
+	len /= 2;
+
+	while (len--)
+	{
+		you = s[a];
+		s[a] = s[k];
+		s[k] = you;
+		a++;
+		k--;
+	}
+}
