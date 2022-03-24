f4f6251560b9aaaaa3d620bb1dbf3727083ae312
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-24 02:00:50 -0700

    Reverse array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,21 @@
- 
+#include "main.h"
+
+/**
+ * reverse_array - function
+ *
+ *@a: array to be reversed
+ *@n: elements number
+ */
+void reverse_array(int *a, int n)
+{
+	int x, y, alpha;
+
+	y = n - 1;
+
+	for (x = 0; x < n / 2; x++)
+	{
+		alpha = a[x];
+		a[x] = a[y];
+		a[y--] = alpha;
+	}
+}
