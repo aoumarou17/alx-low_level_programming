ad3970175ef8fb7e7dde440e6c823a97f08f4abd
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-24 02:08:49 -0700

    String modification

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,21 @@
- 
+#include "main.h"
+
+/**
+ * *string_toupper - function
+ *
+ * @s: string to be modify
+ *
+ * Return: return a string
+ */
+char *string_toupper(char *s)
+{
+	int a = 0;
+
+	while (*(s + a))
+	{
+		if ((*(s + a) >= 97) &&  (*(s + a) <= 122))
+			*(s + a) = *(s + a) - 32;
+		a++;
+	}
+	return (s);
+}
