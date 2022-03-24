9fae7d694c9219084c9b0b00eac3b3107b1df126
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-24 01:23:23 -0700

    The string on

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,1 +1,24 @@
- 
+#include "main.h"
+
+/**
+ * *_strncpy - function
+ *
+ * @dest: destination string
+ * @src: starting string
+ * @n: the number of bytes
+ *
+ * Return: return dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x = 0;
+
+	for (x = 0; x < n && src[x] != 0; x++)
+		dest[x] = src[x];
+	while (x < n)
+	{
+		dest[x] = 0;
+		x++;
+	}
+	return (dest)
+}
