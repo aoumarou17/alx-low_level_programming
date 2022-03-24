2e71dea3074306900a0fd8276baca9d478bf3fa8
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-24 02:29:24 -0700

    String vibes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,1 +1,32 @@
- 
+#include "main.h"
+
+/**
+ * cap_string - function
+ *
+ * @s: string to be modified
+ *
+ *Return: return a string
+ */
+char *cap_string(char *s)
+{
+	int a = 0, b;
+
+	char diff[13] = {9, 10, 32, 33, 34, 40, 41,
+		44, 46, 59, 63, 123, 125};
+
+	while (*(s + a))
+	{
+		for (b = 0; b < 13; b++)
+		{
+			if (a == 0 && s[a] >= 97 && s[a] <= 122)
+				s[a] -= 32;
+			if (s[a - 1] == diff[b])
+			{
+				if ((*(s + a) >= 97) &&  (*(s + a) <= 122))
+					*(s + a) -= 32;
+			}
+		}
+		a++;
+	}
+	return (s);
+}
