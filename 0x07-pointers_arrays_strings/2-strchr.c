21c997bfab2769acefc3f076ea7fd721ad784a87
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-28 10:17:13 -0700

    Some changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,1 +1,22 @@
- 
+#include "main.h"
+
+/**
+ * _strchr - function
+ *
+ * @s: string
+ * @c: character
+ *
+ * Return: return to the pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	unsigned int i = 0;
+
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
+}
