3bdc9ca0446143b1dcb006bde6de2ddc37b7eee6
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-28 10:03:30 -0700

    Pointers in action

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memcpy - function
+ *
+ * @dest: destinantion memory
+ * @src: starting memory
+ * @n: filled byte
+ *
+ * Return: return deste
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		*(dest + x) =  *(src + x);
+
+	return (dest);
+}
