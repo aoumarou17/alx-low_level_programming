e4949c004e53afc8ecfd91c874f64e0dbf07607d
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-28 14:20:19 -0700

    Goal

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -2,8 +2,8 @@
 
 /**
  * set_string - function
- *@s: source adress
- *@to: targetting adress
+ * @s: source adress
+ * @to: targetting adress
  * Return: no return
  */
 void set_string(char **s, char *to)
