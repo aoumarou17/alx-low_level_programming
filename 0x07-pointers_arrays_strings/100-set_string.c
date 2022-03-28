61dd91077464b50c6c30f574c0d2625256e621f0
aoumarou17 <aoumarou17@alaalumni.org>
2022-03-28 12:13:56 -0700

    We cracked it

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include <main.h>
+
+/**
+ * set_string - function
+ *
+ *@s: source adress
+ *@to: targetting adress
+ *
+ * Return: no return
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
