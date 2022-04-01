a71b37bc60d5618edf50cda8baa44603312cd3ea
aoumarou17 <aoumarou17@alaalumni.org>
2022-04-01 12:28:55 -0700

    Fixes

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,29 +13,21 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, result;
+	int i, j, result = 0;
 
-	result = 0;
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
+
+	for (i = 1; i < argc; i++)
 		{
 			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				if (argv[i][j] >= '0' && argv[i][j] <= '9')
-				{
-					continue;
-				}
-				else
+				if (!isdigit(argv[i][j]))
 				{
 					printf("Error\n");
-					return (0);
+					return (1);
 				}
 			}
-			result += atoi(argv[i]);
+			add += atoi(argv[i]);
 		}
-		printf("%d\n", result);
-	} else
-		printf("0\n");
+		printf("%d\n", add);
 		return (0);
 }
